94f3d0284c830b03c3d68fe5e3b51b0165fc75e0
slavikfoxy <oliynik.v2014@gmail.com>
2024-09-05 00:19:54 +0200

    123

--- CheckMissVideo – PDF.py
+++ CheckMissVideo – PDF.py
@@ -498,9 +498,9 @@ if __name__ == "__main__":
     #input("Costume.json",  "https://rt.pornhub.com/playlist/228798371", videoUrlCostume)
     #compare_json_files_by_keywords("MY.json", outputLog, videoUrlMy)
 
-    #down("MY.json");
-    #down("Costume.json");
-    #compare_json_files_by_keywords2()
+    down("MY.json");
+    down("Costume.json");
+    compare_json_files_by_keywords2()
     build_HTML("tmp.json")
     
 
